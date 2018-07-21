9cbe82b7609682bdd2cd1de2bf79ee8c99bdc248
gilbrater <gilbrater1@gmail.com>
2018-07-21 11:33:35 +0100

    Weydem application  setup

---
+++ src/main/java/com/gigoteam/weydem/WeydemApplication.java
@@ -0,0 +1,12 @@
+package com.gigoteam.weydem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeydemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeydemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gigoteam/weydem/WeydemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gigoteam.weydem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeydemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
